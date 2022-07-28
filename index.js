e1df053a6aae0618d29ae26d79f8dd2f06f10c27
duaneo2011 <duaneo2011@gmail.com>
2022-07-28 07:15:00 +0200

    removed the unnesscary index.js file
    removed the unnesscary index.js file

--- index.js
+++
@@ -1,5 +0,0 @@
-const toggleButton = document.getElementsByClassName('custom-toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('custom-navbar-links')[0];
-toggleButton.addEventListener('click', () => {
-	navbarLinks.classList.toggle('active');
-});
